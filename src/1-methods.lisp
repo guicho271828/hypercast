a7214bbab746b6c5c58f3db58d2c2c82a5af505f
Masataro Asai <guicho2.71828@gmail.com>
2016-06-14 11:31:51 +0100

    more methods

--- src/1-methods.lisp
+++ src/1-methods.lisp
@@ -1,9 +1,24 @@
 
 (in-package :hypercast)
 
+#+template
+(defmethod cast ((i ) (type (eql )))
+  (declare (ignorable type))
+  )
+
 (defmethod cast ((code fixnum) (type (eql 'character)))
   (declare (ignorable type))
   (code-char code))
+(defmethod cast ((x string) (type (eql 'character)))
+  (declare (ignorable type))
+  (coerce x 'character))
+(defmethod cast ((x symbol) (type (eql 'character)))
+  ;; string designator
+  (declare (ignorable type))
+  (coerce x 'character))
+(defmethod cast ((x character) (type (eql 'character)))
+  (declare (ignorable type))
+  x)
 
 (defconstant +fixnum-size/16+ (* 16 (ceiling (integer-length most-positive-fixnum) 16)))
 
@@ -18,9 +33,9 @@
 
 ;; from ironclad
 
-(defmethod cast ((i integer) (type (eql :octet-vector)))
+(defmethod cast ((bignum integer) (type (eql :octet-vector)))
   (declare (ignorable type))
-  (let* ((n-bytes (ceiling n-bits 8))
+  (let* ((n-bytes (ceiling (integer-length bignum) 8))
          (octet-vec (make-array n-bytes :element-type '(unsigned-byte 8))))
     (declare (type (simple-array (unsigned-byte 8)) octet-vec))
     (loop for i from (1- n-bytes) downto 0
@@ -28,5 +43,35 @@
           do (setf (aref octet-vec index) (ldb (byte 8 (* i 8)) bignum))
           finally (return octet-vec))))
 
+(defmethod cast ((x sequence) (type (eql 'list)))
+  (declare (ignorable type))
+  (coerce x type))
+
+(defmethod cast ((x sequence) (type (eql 'vector)))
+  (declare (ignorable type))
+  (coerce x type))
+
+(defmethod cast ((x real) (type (eql 'complex)))
+  (declare (ignorable type))
+  (coerce x type))
+(defmethod cast ((x rational) (type (eql 'complex)))
+  (declare (ignorable type))
+  x)
+(defmethod cast ((x complex) (type (eql 'complex)))
+  (declare (ignorable type))
+  x)
+
+(defmacro in-compile-time (&body body)
+  (eval `(progn ,@body)))
+
+(in-compile-time
+  (iter
+    (for type in '(float short-float single-float double-float long-float))
+    (when (first-time-p)
+      (collect 'progn))
+    (collect
+        `(defmethod cast ((x real) (type (eql ',type)))
+           (declare (ignorable type))
+           x))))
 
 

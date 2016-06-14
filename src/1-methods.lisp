c74d9de375c294661f94d100a1ecf83080f783e6
Masataro Asai <guicho2.71828@gmail.com>
2016-06-14 10:47:41 +0100

    octets

--- src/1-methods.lisp
+++ src/1-methods.lisp
@@ -16,3 +16,17 @@
         (setf (aref a j) (ldb (byte 1 j) i))
         (finally (return a))))
 
+;; from ironclad
+
+(defmethod cast ((i integer) (type (eql :octet-vector)))
+  (declare (ignorable type))
+  (let* ((n-bytes (ceiling n-bits 8))
+         (octet-vec (make-array n-bytes :element-type '(unsigned-byte 8))))
+    (declare (type (simple-array (unsigned-byte 8)) octet-vec))
+    (loop for i from (1- n-bytes) downto 0
+          for index from 0
+          do (setf (aref octet-vec index) (ldb (byte 8 (* i 8)) bignum))
+          finally (return octet-vec))))
+
+
+

f29841e52c6c599d8dc33c7f2c77061d637fabd7
Masataro Asai <guicho2.71828@gmail.com>
2016-06-14 07:21:44 +0100

    rebase master automatically

--- bench.sh
+++ bench.sh
@@ -3,6 +3,7 @@
 for b in $(git for-each-ref --format='basename %(refname)' refs/heads/)
 do
     git checkout $(eval $b)
+    git rebase master
     ros ./testscr.ros 2>/dev/null | grep "seconds of real time"
 done
 

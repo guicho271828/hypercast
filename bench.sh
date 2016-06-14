6bf2f20b2f4be255ee66b924bdf0d18e2c29be36
Masataro Asai <guicho2.71828@gmail.com>
2016-06-14 07:24:35 +0100

    fix

--- bench.sh
+++ bench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for b in $(git for-each-ref --format='"basename %(refname)"' refs/heads/)
+for b in $(git for-each-ref --format='%(refname)' refs/heads/)
 do
-    git checkout $(eval $b)
+    git checkout $(basename $b)
     git rebase master
     ros ./testscr.ros 2>/dev/null | grep "seconds of real time"
 done

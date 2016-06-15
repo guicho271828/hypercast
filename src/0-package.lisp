980c53844848d8f3146a2f106214e8197ef99ca4
Masataro Asai <guicho2.71828@gmail.com>
2016-06-15 12:22:34 +0100

    std generic function

--- src/0-package.lisp
+++ src/0-package.lisp
@@ -13,7 +13,6 @@
 ;; blah blah blah.
 
 (defgeneric cast (object type)
-  (:generic-function-class inlined-generic-function)
   (:argument-precedence-order type object))
 
 (defmacro in-compile-time (&body body)

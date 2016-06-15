75fedaa68d18ac3d08802d9533bec9edc2045dd4
Masataro Asai <guicho2.71828@gmail.com>
2016-06-15 10:18:43 +0100

    handle int

--- bench.sh
+++ bench.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+finish (){
+    kill $pid
+    exit
+}
+trap "finish" SIGINT
+
 for b in $(git for-each-ref --format='%(refname)' refs/heads/)
 do
     git checkout $(basename $b)
     git rebase master
     touch src/* t/*
-    ros ./testscr.ros
+    ros ./testscr.ros &
+    pid=$!
+    wait
 done
 
